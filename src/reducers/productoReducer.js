22859ef46c004fdaf8891513812821a7c09b3fac
Manuel <mninahuanca4@gmail.com>
2021-03-20 00:09:26 -0400

    definiendo las primeras acciones en REDUX

--- src/reducers/productoReducer.js
+++ src/reducers/productoReducer.js
@@ -1,3 +1,9 @@
+import {
+    AGREGAR_PRODUCTO,
+    AGREGAR_PRODUCTO_EXITO,
+    AGREGAR_PRODUCTO_ERROR
+ } from '../types/index';
+
 // Cada Reducer tiene su propio state
 const initialState = {
     productos: [],
