589d5075ce5a11735c9dc9d5c525b1a2ff8bb655
Manuel <mninahuanca4@gmail.com>
2021-03-21 18:27:51 -0400

    mostrando los productos en ka interfaz

--- src/components/Productos.js
+++ src/components/Productos.js
@@ -1,16 +1,21 @@
 import React, { Fragment, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { obtenerProductosAction } from '../actions/productoActions';
+import Producto from '../components/Producto';
 
 const Productos = () => {
-    
+
     const dispatch = useDispatch();
 
     useEffect(() => {
         // consultar la api
         const cargarProductos = () => dispatch(obtenerProductosAction());
         cargarProductos();
-    }, [])
+    }, []);
+
+    // Obtener el state
+    const productos = useSelector(state => state.productos.productos);
+
     return (
         <Fragment>
             <h2 className="text-center my-5">Listado de productos</h2>
@@ -24,11 +29,18 @@ const Productos = () => {
                     </tr>
                 </thead>
                 <tbody>
-                    
+                    {productos.length === 0 ? 'No hay productos' : (
+                        productos.map(producto => (
+                            <Producto
+                                key={producto.id}
+                                producto={producto}
+                            />
+                        ))
+                    )}
                 </tbody>
             </table>
         </Fragment>
-     );
+    );
 }
- 
+
 export default Productos;
